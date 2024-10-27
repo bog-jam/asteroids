a4646e30d65135890b2c3f441d2628491458e567
bog-jam <patrick.schofield45@gmail.com>
2024-10-27 18:52:16 -0400

    Added the Draw Player stuff.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from player import Player
 from constants import *
 def main():
     pygame.init()
@@ -9,14 +10,23 @@ def main():
     print (f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
-        pygame.display.flip()
+        
+        player.draw(screen)
 
+        pygame.display.flip()
+        # call tick and calculate dt
+        dt = clock.tick(60) / 1000.0
+    
 
 
 if __name__ == "__main__":
